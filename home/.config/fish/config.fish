abbdfd11be1b5a958f65cafb5dad7e732de2ea87
hoongeun <hoongeun@nexon.co.kr>
2024-07-25 08:59:51 +0900

    chore(fish): add alias

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,5 +1,25 @@
-rvm default
-set -gx TERM "xterm-256color"
+set -gx TERM xterm-256color
 
-alias vi=nvim
-alias vim=nvim
+fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin"
+! set -q MANPATH; and set MANPATH ''
+set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH
+! set -q INFOPATH; and set INFOPATH ''
+set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH
+
+starship init fish | source
+
+# Created by `pipx` on 2024-06-26 07:15:23
+set -gx PATH $PATH /Users/hoongeun/.local/bin
+
+alias tf="terraform"
+alias k="kubectl"
+alias zj="zellij"
+
+function yy
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
